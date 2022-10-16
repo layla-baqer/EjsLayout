1a12db36cc17eb1150738b7146a4bc68fccdb0c5
Layla <layla.baqer97@gmail.com>
2022-10-16 10:14:30 +0300

    Adding animals page with favorite animals

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require("express")
 const app = express()
 PORT = 8000
 
+const ejsLayouts = require("express-ejs-layouts")
+
 // middleware
 app.set("view engine", "ejs")
+app.use(ejsLayouts)
 
 // routes
+// home
+app.get("/", (req, res)=>{
+    res.render("home.ejs")
+})
 
-
+app.get("/animals", (req, res)=>{
+    const favAnimals = ["dolphin", "killer whale", "turtle"]
+    res.render("animals.ejs", {animals: favAnimals})
+})
 
 app.listen(PORT, ()=>{
     console.log("Listening....")
